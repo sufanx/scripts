9ac82800bdf7ac05abf3be25c0a74bf9ad0ec544
sufanx <sufan.x@gmail.com>
2023-02-13 16:34:15 +0800

    完善脚本

--- qx/chuanguan.js
+++ qx/chuanguan.js
@@ -3,23 +3,9 @@ let obj = JSON.parse(body);
 
 if (obj.code === 0) {
     let questions = obj.data.question;
-    console.log(questions);
     questions.forEach((question) => {
         question.answer.forEach((answer) => {
             let answerIndex = atob(answer);
-            // switch(answer){
-            //     case "QQ==" :
-            //         answerIndex="A";
-            //         break;
-            //     case "Qg==" :
-            //         answerIndex="B";
-            //         break;
-            //     case "Qw==" :
-            //         answerIndex="C";
-            //         break;
-            //     default :
-            //         answerIndex="D";
-            // }
             question.option.forEach((option) => {
                 if (option.index === answerIndex) {
                     option.detail = "Y " + option.detail;
